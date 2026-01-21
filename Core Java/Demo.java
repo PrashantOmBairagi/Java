043cf34510de5093449f2db93515888b3e1a6f09
PrashantOmBairagi <prashantbairagi2018@gmail.com>
2026-01-21 17:31:55 +0530

    21-JAN-2026 Switch was updated in JAVA16 and may not require break after every case:

--- Core Java/Demo.java
+++ Core Java/Demo.java
@@ -1,19 +1,41 @@
+//21-JAN-2026 Updated Switch Statement!!
 public class Demo
 {
     public static void main(String a[])
     {
-        int n = 6;
-        switch(n)
+        int n = 1;
+        //OLD 
+        // switch(n)
+        // {
+        //     case 1: System.out.println("Monday"); break;
+        //     case 2: System.out.println("Tuesday"); break;
+        //     case 3: System.out.println("Wednesday"); break;
+        //     case 4: System.out.println("Thursday"); break;
+        //     case 5: System.out.println("Friday"); break;
+        //     case 6: System.out.println("Saturday"); break;
+        //     case 7: System.out.println("Sunday"); break;
+        //     default: System.out.println("invalid day"); 
+        // }
+
+            // Break can be replaced by doing (: to ->)
+            //switch now can also return values
+            // case 2 -> System.out.println("Tuesday"); 
+            // case 3 -> System.out.println("Wednesday"); 
+            // case 4 -> System.out.println("Thursday"); 
+
+        String result = switch(n)
         {
-            case 1: System.out.println("Monday"); break;
-            case 2: System.out.println("Tuesday"); break;
-            case 3: System.out.println("Wednesday"); break;
-            case 4: System.out.println("Thursday"); break;
-            case 5: System.out.println("Friday"); break;
-            case 6: System.out.println("Saturday"); break;
-            case 7: System.out.println("Sunday"); break;
-            default: System.out.println("invalid day"); 
-        }
+            case 1 -> "Monday"; 
+            case 2 -> "Tuesday"; 
+            case 3 -> "Wednesday"; 
+            case 4 -> "Thursday"; 
+            case 5 -> "Friday"; 
+            case 6 -> "Saturday"; 
+            case 7 -> "Sunday"; 
+            default ->  "invalid";
+        };   // now semi colon is must.
+
+        System.out.println(result);
 
     }
 }
