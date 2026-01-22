6ec83d663de8e9744e2012c4f5a34fce383058cc
PrashantOmBairagi <prashantbairagi2018@gmail.com>
2026-01-22 19:18:43 +0530

    22-JAN-2026 Found out the syntax of loops is exactly similar as CPP

---
+++ Core Java/loop.java
@@ -0,0 +1,12 @@
+public class loop
+{
+    public static void main(String a[])
+    {
+        short i =1;
+        while(true)
+        {
+            System.out.println("Hi" + i);
+            i++;
+        }
+    }
+}
