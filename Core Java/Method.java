dcc58051ecbf67f94d08b624d97419edd7f20150
PrashantOmBairagi <prashantbairagi2018@gmail.com>
2026-01-22 20:02:37 +0530

    22-JAN-2026 Understanding Methods And Method overlaodingin JAVA

---
+++ Core Java/Method.java
@@ -0,0 +1,38 @@
+//  22-JAN-2026 METHOD & METHOD OVERLOADING:
+
+class Calculator
+{
+    public boolean add(int a,int b)
+    {
+        System.out.println("in CalC !");
+        System.out.println(a+b);
+        return true;
+    }
+    public Double add(Double a,int b)
+    {
+        System.out.println("in CalC !");
+        System.out.println(a+b);
+        return a+b;
+    }
+    public boolean add(int a,int b,int c)
+    {
+        System.out.println("in CalC !");
+        System.out.println(a+b+c);
+        return true;
+    }
+    // we can have many methods of same name in java is called method overloading. but they must require different argument types exact copy wont work, thats OBV I guess!!
+}
+
+public class Method
+{
+    public static void main(String[] args)
+    {
+        int y=6;
+        int z=9;
+
+        System.out.println("in main!");
+
+        Calculator obj = new Calculator();
+        System.out.println("in main!  " + obj.add(y,z));
+    }
+}
