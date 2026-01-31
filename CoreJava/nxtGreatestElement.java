0526177933448897b7f7f9b23f8d11b5d7d94579
PrashantOmBairagi <prashantbairagi2018@gmail.com>
2026-01-31 20:46:42 +0530

    31-JAN-2026 Transpose of matrix

--- CoreJava/nxtGreatestElement.java
+++ CoreJava/nxtGreatestElement.java
@@ -1,4 +1,27 @@
 package CoreJava;
 
 public class nxtGreatestElement {
+    static void main() {
+        int [] arr = {12,8,60,37,2,49,16,28,21};
+        int n = arr.length;
+        int [] ans = new int[n];
+        int mx = -1;
+        for (int i = n-1; i >=0 ; i--) {
+            if(arr[i]>mx) {
+                ans[i] = mx;
+                mx = arr[i];
+
+            }else ans[i] = mx;
+
+        }
+
+        for (int ele: arr) {
+            System.out.print(ele+" ");
+        }
+        System.out.println();
+        for (int ele: ans) {
+            System.out.print(ele+" ");
+        }
+        System.out.println();
+    }
 }
