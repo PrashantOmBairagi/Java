0ab382c44b08131871857288c9ba2bc6870cdddf
PrashantOmBairagi <prashantbairagi2018@gmail.com>
2026-02-05 22:20:16 +0530

    05-FEB-2026 Practicing string IsomorphicStrings of Leetcode 205.

---
+++ CoreJava/IsomorphicStrings205.java
@@ -0,0 +1,30 @@
+package CoreJava;
+import java.util.*;
+public class IsomorphicStrings205 {
+    static void main() {
+        String s = "egcd";
+        String t = "adfd";
+
+        System.out.print(isIsomorphic(s,t));
+    }
+    public static boolean isIsomorphic(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        int[] mapST = new int[256];
+        int[] mapTS = new int[256];
+
+        for (int i = 0; i < s.length(); i++) {
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+
+            if (mapST[c1] == 0 && mapTS[c2] == 0) {
+                mapST[c1] = c2;
+                mapTS[c2] = c1;
+            }
+            else if (mapST[c1] != c2 || mapTS[c2] != c1) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
