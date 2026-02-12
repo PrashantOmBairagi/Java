761a0802c0dd34d9f23bf482dea3f4975cc4deb4
PrashantOmBairagi <prashantbairagi2018@gmail.com>
2026-02-12 19:15:15 +0530

    11-FEB-2026 LEETCODE69 -Its great that i found out binary search can also be used in guess work. Obviously BS lowers down search space very Precisely and effectively for real.

--- CoreJava/SquareRoot69.java
+++ CoreJava/SquareRoot69.java
@@ -9,7 +9,6 @@ public class SquareRoot69 {
         int hi = x;
         while(lo<=hi){
             int mid = lo + (hi-lo)/2;
-
             if(mid<=x/mid && (mid+1)>x/(mid+1)) return mid;
             else if(mid< x/mid) lo = mid+1;
             else hi = mid-1;
