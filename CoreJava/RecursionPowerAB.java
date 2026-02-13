8f58b81acd51badbddee61ee732b56f7f813febf
PrashantOmBairagi <prashantbairagi2018@gmail.com>
2026-02-13 19:33:41 +0530

    13-FEB-2026 Revisiting recursion , its always fun to work with recursions.

---
+++ CoreJava/RecursionPowerAB.java
@@ -0,0 +1,19 @@
+package CoreJava;
+
+import java.util.Scanner;
+
+public class RecursionPowerAB {
+    public static int power(int a,int b){
+        if(b==0) return 1;
+        if(b==1) return a;
+        return a * power(a,b-1);
+    }
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number and power respectively : ");
+        int a , b;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        System.out.println(power(a,b));
+    }
+}
