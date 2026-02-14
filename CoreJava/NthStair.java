f8eae5317ee399eca08919c90d283590a458b8b1
PrashantOmBairagi <prashantbairagi2018@gmail.com>
2026-02-14 14:21:59 +0530

    14-FEB-2026 Revisiting recursion : famous staircase analogy for fibonacci series. TBH I got it really well in first time with cpp.

--- CoreJava/NthStair.java
+++ CoreJava/NthStair.java
@@ -2,7 +2,7 @@ package CoreJava;
 
 public class NthStair {
     public static int stair(int n){
-        if(n<=1) return 1;
+        if(n<=2) return n;
         return stair(n-1)+stair(n-2);
     }
     static void main() {
