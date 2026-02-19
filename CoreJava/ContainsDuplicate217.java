b2937f3e216602ba2d4b0da99b575da6b765411c
PrashantOmBairagi <prashantbairagi2018@gmail.com>
2026-02-19 22:11:11 +0530

    19-FEB-2026 Solved LEETCODE217, Best time Complexity i can get is by using built in sort. otherwise without bit manipulation Big O(n^2) is least i can get, gives TLE

---
+++ CoreJava/ContainsDuplicate217.java
@@ -0,0 +1,18 @@
+package CoreJava;
+
+import java.util.Arrays;
+
+public class ContainsDuplicate217 {
+    public static boolean containsDuplicate(int[] nums){
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length-1; i++) {
+            if (nums[i]==nums[i+1]) return true;
+        }
+        return false;
+    }
+
+    static void main() {
+        int[] arr = {1,2,3,4,2};
+        System.out.println(containsDuplicate(arr));
+    }
+}
