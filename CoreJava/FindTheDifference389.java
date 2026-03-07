44426c4c90afcdcc6ec2d7cecdecb6c9e1ac9c07
PrashantOmBairagi <prashantbairagi2018@gmail.com>
2026-03-07 14:44:25 +0530

    "07-March-2026 Leetcode389"

--- CoreJava/FindTheDifference389.java
+++ CoreJava/FindTheDifference389.java
@@ -1,4 +1,18 @@
 package CoreJava;
 
 public class FindTheDifference389 {
+    public static char findTheDifference(String s, String t){
+        int sum = 0;
+        for(int i = 0; i < s.length(); i++){
+            sum += s.charAt(i);
+            sum-=t.charAt(i);
+        }
+        sum -= t.charAt(t.length()-1);
+        return (char)(-sum);
+    }
+    static void main() {
+        String s = "abcd";
+        String t = "abcde";
+        System.out.println(findTheDifference(s,t));
+    }
 }
