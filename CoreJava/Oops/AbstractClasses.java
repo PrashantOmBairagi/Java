0706bdc80fa09faadd218f7786026b408c8f74ba
PrashantOmBairagi <prashantbairagi2018@gmail.com>
2026-03-08 12:50:24 +0530

    08-March-2026 Learn abstraction and interface.

--- CoreJava/Oops/AbstractClasses.java
+++ CoreJava/Oops/AbstractClasses.java
@@ -8,12 +8,65 @@ class Child extends Parent {
         System.out.println("Child abstractMethod");
     }
 }
+abstract class Plane2 {
+    public abstract void takeOff();
+
+    public abstract void fly();
+    public abstract void landing();
+}
+
+class CargoPlane2 extends Plane2{
+    public void takeOff() {
+        System.out.println("Cargo-Plane Take off...");
+    }
+    public void fly() {
+        System.out.println("Cargo-Plane Flying...");
+    }
+    public void landing(){
+        System.out.println("Cargo-Plane Landing...");
+    }
+}
+
+class FighterPlane2 extends Plane2{
+    public void takeOff() {
+        System.out.println("Fighter-Plane Take off...");
+    }
+    public void fly(){
+        System.out.println("Fighter-Plane Flying...");
+    }
+    public void landing(){
+        System.out.println("Fighter-PlaneLanding...");
+    }
+}
+class PassengerPlane2 extends Plane2{
+    public void takeOff() {
+        System.out.println("Passenger-Plane Take off...");
+    }
+    public void fly(){
+        System.out.println("Passenger-Plane Flying...");
+    }
+    public void landing(){
+        System.out.println("Passenger-Plane Landing...");
+    }
+}
+
+class Airport2 {
+    public void allowPlane(Plane2 obj) {
+        obj.takeOff();
+        obj.fly();
+        obj.landing();
+        System.out.println();
+    }
+}
 
 public class AbstractClasses {
-
-
     static void main() {
         Parent p1 = new Child();
         p1.abstractMethod();
+
+        Airport2 a1 = new Airport2();
+        a1.allowPlane(new PassengerPlane2());
+        a1.allowPlane(new CargoPlane2());
+        a1.allowPlane(new FighterPlane2());
     }
 }
