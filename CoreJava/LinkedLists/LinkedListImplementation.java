2b644298ff76e8bcbcd2c69ad030b5d0b31c07a5
PrashantOmBairagi <prashantbairagi2018@gmail.com>
2026-03-16 21:13:36 +0530

    16-March-2026 Making User Defined data type of LL. ADD InsertAt method.

--- CoreJava/LinkedLists/LinkedListImplementation.java
+++ CoreJava/LinkedLists/LinkedListImplementation.java
@@ -14,6 +14,30 @@ class LinkedLists{
         }
         size++;
     }
+    static void insertAt(int idx , int val){
+        if(idx==0){
+            addAtStart(val);
+            return;
+        }
+        if(idx==size){
+            addAtEnd(val);
+            return;
+        }
+        if(idx>size){
+            System.out.println("Invalid Input!!");
+            return;
+        }
+        Node newNode = new Node(val);
+        Node dummy = head;
+        for(int i=0;i<idx;i++){
+            if(i==idx-1){
+                newNode.next = dummy.next;
+                dummy.next = newNode;
+            }
+            dummy =dummy.next;
+        }
+        size++;
+    }
     static void addAtStart(int val){
         Node temp = new Node(val);
         if(head==null){
@@ -49,7 +73,7 @@ public class LinkedListImplementation {
         LinkedLists.addAtStart(10);
         LinkedLists.addAtStart(7);
         LinkedLists.addAtStart(8);
-
+        LinkedLists.insertAt(3,70);
         System.out.println("Size = " +LinkedLists.size);
         LinkedLists.print();
 
