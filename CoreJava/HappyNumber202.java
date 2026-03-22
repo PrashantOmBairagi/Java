e577cd39e75a98fcf50a6a7d77135e6ec8503964
PrashantOmBairagi <prashantbairagi2018@gmail.com>
2026-03-22 20:26:21 +0530

    22-March-2026 LEETCODE 202, Happy Number is very famous for Floyds Cycle detection algo.

---
+++ CoreJava/HappyNumber202.java
@@ -0,0 +1,41 @@
+package CoreJava;
+
+public class HappyNumber202 {
+    // Floyds Cycle detection algo
+    public static boolean isHappy(int n) {
+        int slow = n;
+        int fast = getNext(n);
+        while (fast != 1 && slow != fast) {
+            slow = getNext(slow);
+            fast = getNext(getNext(fast));
+        }
+        return fast == 1;
+    }
+    static int getNext(int n){
+        int sum = 0;
+        while (n != 0){
+            int digit = n % 10;
+            sum += digit * digit;
+            n = n/10;
+        }
+        return sum;
+    }
+    // M-II
+    public static boolean isHappy2(int n) {
+        while(n != 1 && n != 4){
+            int sum = 0;
+            while(n != 0){
+                int digit = n%10;
+                sum += digit * digit;
+                n/=10;
+            }
+            n = sum;
+        }
+        return n == 1;
+    }
+    static void main() {
+        int n = 19;
+        System.out.println(isHappy(n));
+        System.out.println(isHappy2(n));
+    }
+}
