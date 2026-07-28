3a04cc6a302a32eac68b00f80fe5725f0b88e273
PrashantOmBairagi <prashantbairagi2018@gmail.com>
2026-07-28 19:06:07 +0530

    28-july-2026 ArrayListPractice

---
+++ CoreJava/ArrayListPractice.java
@@ -0,0 +1,21 @@
+package CoreJava;
+
+import java.util.*;
+
+public class ArrayListPractice {
+
+
+
+    static void main() {
+        var list = new ArrayList<String>();
+        var list2 = new ArrayList<String>();
+
+
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println(list);
+
+
+    }
+}
